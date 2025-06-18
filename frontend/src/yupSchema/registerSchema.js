afcea9b14fbc00403751e58c6ba2e0eec3eace8b
Wasid786 <114327679+Wasid786@users.noreply.github.com>
2025-06-18 23:47:25 +0530

    register form validation added

---
+++ frontend/src/yupSchema/registerSchema.js
@@ -0,0 +1,10 @@
+import * as yup from 'yup'
+export const registerSchema = yup.object({
+    school_name: yup.string().min(8,"School name must contians 8 Characters.").required("School name is required"),
+    email: yup.string().email("It must be an Email.").required("Email is Required."),
+    owner_name:yup.string().min(3,"Owner name must have 3 Characters").required("It is required Field"),
+    password: yup.string().min(8,"Password name must have 8 Characters").required("Password is required"),
+    confirm_password:yup.string().oneOf([yup.ref('password')], "Confirm Password Must Match With Password").required("Confirm password Required")
+    
+
+})
